d7903f7f2f7544a4409e82f838f26890d60bbedc
William Entriken <github.com@phor.net>
2016-02-04 14:50:56 -0500

    move appdelegate to swift

---
+++ Pods/Target Support Files/RaptureXML/RaptureXML-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "RXMLElement.h"
+
+FOUNDATION_EXPORT double RaptureXMLVersionNumber;
+FOUNDATION_EXPORT const unsigned char RaptureXMLVersionString[];
+
---
+++ Pods/Target Support Files/UIImage+SVG/UIImage+SVG-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "PocketSVG.h"
+#import "SVGImageCache.h"
+#import "UIImage+SVG.h"
+
+FOUNDATION_EXPORT double UIImage_SVGVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIImage_SVGVersionString[];
+
